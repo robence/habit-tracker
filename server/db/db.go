816d4edb29c6a30eadcf63705cd65f0e5c42f9e5
rozsabence <robence@gmail.com>
2020-02-02 12:07:25 +0100

    cleanup some code

--- server/db/db.go
+++ server/db/db.go
@@ -2,10 +2,11 @@ package db
 
 import (
 	"context"
+	"log"
+
 	"github.com/robence/habit-tracker/model"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"log"
 )
 
 type DB interface {
@@ -19,7 +20,6 @@ type MongoDB struct {
 func New(client *mongo.Client) *MongoDB {
 	habits := client.Database("habits").Collection("habits")
 	return &MongoDB{
-		// collection: programmers,
 		collection: habits,
 	}
 }
--- server/db/db_test.go
+++ server/db/db_test.go
@@ -11,12 +11,9 @@ func TestFilter(t *testing.T) {
 	mongoDB := MongoDB{}
 
 	got := mongoDB.filter()
-	// got := "a"
-	// want := "b"
 	want := bson.D{}
 
 	if !reflect.DeepEqual(got, want) {
-	// if "Hello Dude!" != "Hello Dude!" {
 		t.Errorf("filter() got = %v, want %v", got, want)
 	}
 }
